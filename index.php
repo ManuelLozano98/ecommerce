33a76fc87b214da6cf652bbccacf75b0adb8e45b
ManuelLozano98 <lololozano1998@gmail.com>
2025-10-15 18:11:16 +0200

    chore: configure middlewares and load API routes

--- index.php
+++ index.php
@@ -1,2 +1,21 @@
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
+
+
+use Slim\Factory\AppFactory;
+use App\Middleware\ApiMiddleware;
+use App\Middleware\ServiceMiddleware;
+use Middlewares\TrailingSlash;
+
+$app = AppFactory::create();
+
+$app->setBasePath('/Ecommerce'); // Set the base path to your project folder
+
+$app->add(new ApiMiddleware($app->getResponseFactory()));
+$app->add(new ServiceMiddleware($app->getResponseFactory()));
+
+$app->add(new TrailingSlash(true)); // Prevent issues if user forgets the trailing slash by adding it
+
+(require __DIR__ . '/routes/api.php')($app);
+
+$app->run();
