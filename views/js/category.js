3da65d43dfa9c9a01a1c69ddb0c1834d7117fd08
ManuelLozano98 <lololozano1998@gmail.com>
2025-10-20 22:18:47 +0200

    feat: implement async functions insert, delete, and edit

--- views/js/category.js
+++ views/js/category.js
@@ -30,3 +30,62 @@ function loadModalByURL() {
     $("#modal-default").modal("show");
   }
 }
+
+async function insert() {
+  const form = getById("form");
+  const category = getJSONForm(form);
+  const { data, error } = await apiRequest("api/categories", {
+    method: "POST",
+    body: category,
+  });
+  if (data) {
+    notifySuccessResponse(API_MSGS.Created);
+    getCategories();
+  }
+  if (error) {
+    notifyErrorResponse(error);
+  }
+}
+
+async function deleteItem(id) {
+  const msg =
+    "<p style='color: red'>Please note that this will also remove associated products.</p>";
+  const response = await getDeleteMsg(msg);
+  if (response.isConfirmed) {
+    const { data, error } = await apiRequest(`api/categories/${id}`, {
+      method: "DELETE",
+    });
+    if (data) {
+      notifySuccessResponse(API_MSGS.Deleted);
+      getCategories();
+    }
+    if (error) {
+      notifyErrorResponse(error);
+    }
+  }
+}
+
+async function edit() {
+  const editForm = getById("form-edit");
+  let categoryForm = getForm(editForm);
+  categoryForm.active === "on"
+    ? (categoryForm.active = 1)
+    : (categoryForm.active = 0);
+
+  const category = JSON.stringify(categoryForm);
+  const { data, error } = await apiRequest(
+    `api/categories/${categoryForm.id}`,
+    {
+      method: "PUT",
+      body: category,
+    }
+  );
+  if (data) {
+    notifySuccessResponse(API_MSGS.Updated);
+    getCategories();
+  }
+  if (error) {
+    console.log(error);
+    notifyErrorResponse(error);
+  }
+}
