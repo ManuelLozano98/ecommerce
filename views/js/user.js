aad33b259f3c613f6bfb0dbe0a9119167751af96
ManuelLozano98 <lololozano1998@gmail.com>
2025-10-30 19:01:33 +0100

    feat: implement user CRUD actions in js

---
+++ views/js/user.js
@@ -0,0 +1,324 @@
+$(document).ready(function () {
+  getUsers();
+  loadEditForm();
+  $("#form-edit").on("submit", function (e) {
+    e.preventDefault();
+    const form = getById("form-edit");
+    if (checkForm(form)) {
+      edit();
+    }
+  });
+  $("#form").on("submit", function (e) {
+    e.preventDefault();
+    const form = getById("form-edit");
+    if (checkForm(form)) {
+      insert();
+    }
+  });
+  loadDeleteButton();
+});
+
+function getUsers() {
+  let loaderTimeout;
+  let loaderShownAt = null;
+  const MIN_VISIBLE_TIME = 400; // ms
+  const DEBOUNCE_DELAY = 200; // ms
+
+  $("#tableUsers").DataTable({
+    serverSide: true,
+    processing: false,
+    ajax: function (data, callback, settings) {
+      loaderTimeout = setTimeout(() => {
+        $(".loader").show();
+        loaderShownAt = Date.now();
+      }, DEBOUNCE_DELAY);
+
+      $.ajax({
+        url: "api/users/detailed",
+        method: "GET",
+        data: data,
+        success: function (response) {
+          callback({
+            draw: response.draw,
+            recordsTotal: response.recordsTotal,
+            recordsFiltered: response.recordsFiltered,
+            data: response.data,
+          });
+        },
+        error: function (xhr, status, error) {
+          console.error("An error occurred while loading data:", error);
+          callback({
+            draw: data.draw,
+            recordsTotal: 0,
+            recordsFiltered: 0,
+            data: [],
+          });
+          toastr.error(error);
+        },
+        complete: function () {
+          clearTimeout(loaderTimeout);
+          if (!loaderShownAt) return;
+
+          const elapsed = Date.now() - loaderShownAt;
+          const remainingTime = MIN_VISIBLE_TIME - elapsed;
+
+          if (remainingTime > 0) {
+            setTimeout(() => {
+              $(".loader").hide();
+              loaderShownAt = null;
+            }, remainingTime);
+          } else {
+            $(".loader").hide();
+            loaderShownAt = null;
+          }
+        },
+      });
+    },
+    ...getSettingsDataTable(),
+    buttons: getButtonsDataTable(),
+    dom: getDomStyleDataTable(),
+    columns: [
+      { data: "id" },
+      {
+        data: "name",
+        render: function (data) {
+          return truncateText(data);
+        },
+      },
+      {
+        data: "email",
+        render: function (data) {
+          return truncateText(data);
+        },
+      },
+      {
+        data: "username",
+        render: function (data) {
+          return truncateText(data);
+        },
+      },
+      {
+        data: "phone",
+      },
+      {
+        data: "image",
+        render: function (data) {
+          let result = "No image found";
+          if (data !== "") {
+            result = `<img alt="${data.substring(
+              0,
+              data.indexOf(".")
+            )}" src="uploads/images/${data}" width="100px" height="100px" object-fit:cover;/>`;
+          }
+          return result;
+        },
+      },
+      {
+        data: "address",
+        render: function (data) {
+          return truncateText(data);
+        },
+      },
+      {
+        data: "document",
+      },
+      {
+        data: "document_name",
+      },
+      {
+        data: "verification_token",
+        render: function (data) {
+          return truncateText(data);
+        },
+      },
+      { data: "token_expires_at" },
+      { data: "registration_date" },
+      {
+        data: "active",
+        render: function (data, type, row) {
+          return data === 1 ? "Yes" : "No";
+        },
+      },
+      getActionsColumnDataTable(),
+    ],
+  });
+}
+function loadEditForm() {
+  $("#tableUsers").on("click", ".edit-button", function () {
+    let row = $(this).closest("tr");
+    let table = $("#tableUsers").DataTable();
+    if (row.hasClass("child")) {
+      row = row.prev(); // needed for responsive tables
+    }
+    let data = table.row(row).data();
+    $("#user_id").val(data.id);
+    $("#edit-name").val(data.name);
+    $("#edit-username").val(data.username);
+    $("#edit-phone").val(data.phone);
+    $("#edit-address").val(data.address);
+    $("#edit-email").val(data.email);
+    $("#edit-document").val(data.document);
+    const $documentId = data.document_type_id;
+    loadDocumentTypes("edit-document_type", $documentId);
+    for (let element of document.getElementById("edit-document_type")
+      .children) {
+      // Find the document
+      element.removeAttribute("selected");
+      if (data.document_type === element.value) {
+        element.setAttribute("selected", true);
+      }
+    }
+    if (data.image !== "") {
+      $("#edit-img").show();
+      $("#edit-img").attr("src", "uploads/images/" + data.image);
+    } else {
+      $("#edit-img").hide();
+    }
+    $("#customSwitch1").prop("checked", data.active === 1);
+  });
+}
+
+async function loadDocumentTypes(selectHtml, id = "") {
+  if (document.getElementById(selectHtml).children.length <= 0) {
+    const { data, error } = await apiRequest(`api/users/documentType/`, {
+      method: "GET",
+    });
+    if (data) {
+      let select = document.getElementById(selectHtml);
+      for (let index = 0; index < data.data.length; index++) {
+        let option = document.createElement("option");
+        option.text = data.data[index].name;
+        option.value = data.data[index].id;
+        if (id && id === data.data.id) {
+          option.selected = true;
+        }
+        select.appendChild(option);
+      }
+    }
+    if (error) {
+      notifyErrorResponse(error);
+    }
+  }
+}
+async function insert() {
+  const form = new FormData(getById("form"));
+  let active = form.get("active");
+  if (active === "on") {
+    form.set("active", "1");
+  } else {
+    form.set("active", "0");
+  }
+  form.delete("image");
+  const user = Object.fromEntries(form.entries());
+
+  const userBody = JSON.stringify(user);
+  const { data, error } = await apiRequest(`api/users/`, {
+    method: "POST",
+    body: userBody,
+  });
+  if (data) {
+    const imageFile = getById("form").image.files[0];
+    if (imageFile) {
+      const imageForm = new FormData();
+      imageForm.append("image", imageFile);
+      const { dataImage, errorImage } = await fetch(
+        `api/users/${data.data.id}/image`,
+        {
+          method: "POST",
+          body: imageForm,
+        }
+      );
+      if (dataImage) {
+        notifySuccessResponse(API_MSGS.Updated);
+        getUsers();
+        return;
+      }
+      if (errorImage) {
+        notifyErrorResponse(errorImage);
+        return;
+      }
+    }
+    if (error) {
+      notifyErrorResponse(error);
+    }
+    notifySuccessResponse(API_MSGS.Updated);
+    getUsers();
+  }
+  if (error) {
+    notifyErrorResponse(error);
+  }
+}
+async function edit() {
+  const $id = $("#user_id").val();
+  const form = new FormData(getById("form-edit"));
+  let active = form.get("active");
+  if (active === "on") {
+    form.set("active", "1");
+  } else {
+    form.set("active", "0");
+  }
+  form.delete("image");
+  const user = Object.fromEntries(form.entries());
+
+  const userBody = JSON.stringify(user);
+  const { data, error } = await apiRequest(`api/users/${$id}/`, {
+    method: "PUT",
+    body: userBody,
+  });
+  if (data) {
+    const imageFile = getById("form-edit").image.files[0];
+    if (imageFile) {
+      const imageForm = new FormData();
+      imageForm.append("image", imageFile);
+      const { dataImage, errorImage } = await fetch(
+        `api/users/${data.data.id}/image`,
+        {
+          method: "POST",
+          body: imageForm,
+        }
+      );
+      if (dataImage) {
+        notifySuccessResponse(API_MSGS.Updated);
+        getUsers();
+        return;
+      }
+      if (errorImage) {
+        notifyErrorResponse(errorImage);
+        return;
+      }
+    }
+    if (error) {
+      notifyErrorResponse(error);
+    }
+    notifySuccessResponse(API_MSGS.Updated);
+    getUsers();
+  }
+  if (error) {
+    notifyErrorResponse(error);
+  }
+}
+async function deleteItem(id) {
+  const response = await getDeleteMsg();
+  if (response.isConfirmed) {
+    const { data, error } = await apiRequest(`api/users/${id}`, {
+      method: "DELETE",
+    });
+    if (data) {
+      notifySuccessResponse(API_MSGS.Deleted);
+      getUsers();
+    }
+    if (error) {
+      notifyErrorResponse(error);
+    }
+  }
+}
+
+function loadDeleteButton() {
+  document.addEventListener("click", function (e) {
+    let deleteBtn = e.target.closest('[id^="btn-delete_"]');
+    if (deleteBtn) {
+      let id = deleteBtn.id.split("_")[1];
+      deleteItem(id);
+    }
+  });
+}
