c5a65fac1ea142c6a86118e09259f30be992aa50
ManuelLozano98 <lololozano1998@gmail.com>
2025-11-05 20:08:07 +0100

    feat: add service methods to fetch usersroles by user, by user-role, and detailed JSON

--- services/UserRoleService.php
+++ services/UserRoleService.php
@@ -51,6 +51,52 @@ class UserRoleService
         }
         return $userRole;
     }
+    public function getUserRolesbyUserId($userId)
+    {
+        $usersRoles = UserRole::findByUserId($userId);
+        if (!$usersRoles) {
+            throw new NotFoundException("The user was not found or not exists");
+        }
+        return $usersRoles;
+    }
+
+    public function getUserRolebyUserIdAndRoleId($userId, $roleId)
+    {
+        $usersRoles = UserRole::findByUserIdAndRoleId($userId, $roleId);
+        if (!$usersRoles) {
+            throw new NotFoundException("The user was not found or not exists");
+        }
+        return $usersRoles[0];
+    }
+
+    public function getUserRolesDetailedJSON()
+    {
+        $usersRoles = UserRole::getAll();
+        $data = [];
+        foreach ($usersRoles as $key => $value) {
+            $data[$key] = $value->toArray();
+            $data[$key]["username"] = $this->userService->getUser($value->getUserId())->getUsername();
+            $data[$key]["name"] = $this->roleService->getRole($value->getRoleId())->getName();
+        }
+        $json = [];
+        foreach ($data as $entry) {
+            $user = $entry["username"];
+            if (!isset($json[$user])) {
+                $json[$user] = [
+                    "user_id" => $entry["user_id"],
+                    "username" => $user,
+                    "roles" => []
+                ];
+            }
+            $json[$user]["roles"][] = [
+                "id" => $entry["id"],
+                "role_id" => $entry["role_id"],
+                "name" => $entry["name"]
+            ];
+        }
+        $json = json_encode(["data" => array_values($json)], true);
+        return $json;
+    }
 
     public function hasAdminRole($user)
     {
