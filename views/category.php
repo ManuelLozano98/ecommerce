1c020f0f0559c8af53dd1ca3770f99279faef281
ManuelLozano98 <lololozano1998@gmail.com>
2026-02-27 20:11:55 +0100

    feat: implement category catalog view with filters, pagination, and search functionality

---
+++ views/category.php
@@ -0,0 +1,256 @@
+    <?php
+    require_once __DIR__ . '/layout/header.php';
+    ?>
+    <title><?php echo SITE ?></title>
+    <link rel="stylesheet" href="<?php echo ROOT ?>/assets/styles/web.css">
+    <!-- Ion Slider -->
+    <link rel="stylesheet" href="<?php echo ADMINLTE ?>plugins/ion-rangeslider/css/ion.rangeSlider.min.css">
+    <link rel="stylesheet" href="<?php echo ROOT ?>/assets/styles/index.css">
+
+    <?php require_once __DIR__ . '/layout/endheader.php'; ?>
+
+    <body>
+        <div class="wrapper">
+            <?php
+            require_once __DIR__ . "/layout/navbar.php";
+            require_once __DIR__ . '/../config/constants.php';
+            $user = $_SESSION["user"] ?? NULL;
+            ?>
+            <aside class="main-sidebar sidebar-dark-primary elevation-4">
+                <!-- Brand Logo -->
+                <a href="./" class="brand-link">
+                    <img src="<?php echo ADMINLTE ?>dist/img/AdminLTELogo.png" alt="AdminLTE Logo"
+                        class="brand-image img-circle elevation-3" style="opacity: .8">
+                    <span class="brand-text font-weight-light"><?php echo SITE ?></span>
+                </a>
+                <!-- Sidebar -->
+                <div class="sidebar">
+
+                    <!-- Sidebar Menu -->
+                    <nav class="mt-2" id="menu">
+                        <h4>Categories</h4>
+                        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+                            <?php
+                            foreach ($categories as $category): ?>
+                                <li class="nav-item">
+                                    <a href="<?php echo strtolower($category->getName()) ?>" class="nav-link">
+                                        <i class="nav-icon fa-solid fa-<?php echo lcfirst($category->getName()[0]); ?>"></i>
+                                        <p>
+                                            <?php echo $category->getName() ?>
+                                        </p>
+                                    </a>
+                                </li>
+                            <?php endforeach; ?>
+
+                        </ul>
+                    </nav>
+                    <!-- /.sidebar-menu -->
+                </div>
+                <!-- /.sidebar -->
+            </aside>
+            <!-- Content Wrapper. Contains page content -->
+            <div class="content-wrapper">
+                <!-- Content Header (Page header) -->
+                <section class="content-header">
+                    <div class="container-fluid">
+                        <form id="filter-form">
+                            <div class="row mb-2">
+                                <div class="col-12 col-md-6">
+                                    <h1>Products</h1>
+                                    <div id="search">
+                                        <input type="text" class="form-control search-form search-product" name="search" placeholder="Search any product" value="">
+                                    </div>
+                                </div>
+                                <div class="col-12 col-md-6">
+                                    <h1>Filters</h1>
+                                    <div class="filters d-flex flex-wrap gap-2 align-items-center">
+                                        <!-- Advanced price filter -->
+                                        <div class="dropdown d-inline-block">
+                                            <button
+                                                class="px-3 py-2 rounded-pill text-nowrap bg-secondary border small fw-medium text-center dropdown-toggle text-white"
+                                                type="button"
+                                                data-toggle="dropdown"
+                                                aria-expanded="false">
+                                                Price
+                                            </button>
+                                            <ul class="dropdown-menu p-3" style="min-width: 220px;">
+                                                <input id="slider" type="text" name="range_price[]" value="">
+                                            </ul>
+                                        </div>
+
+                                        <!-- Advanced review filter -->
+                                        <div class="dropdown d-inline-block">
+                                            <button
+                                                class="px-3 py-2 rounded-pill text-nowrap bg-secondary border small fw-medium text-center dropdown-toggle text-white"
+                                                type="button"
+                                                data-toggle="dropdown"
+                                                aria-expanded="false">
+                                                Score
+                                            </button>
+                                            <ul class="dropdown-menu p-3" style="min-width: 220px;">
+                                                <?php for ($i = 1; $i <= 5; $i++): ?>
+                                                    <li>
+                                                        <div class="form-check">
+                                                            <input class="custom-control-input custom-control-input-secondary custom-control-input-outline" type="checkbox" name="scores[]" value="<?php echo $i ?>" id="review<?php echo $i ?>">
+                                                            <label for="review<?php echo $i ?>" class="custom-control-label font-weight-normal"> <?php echo $i ?> stars</label>
+                                                        </div>
+                                                    </li>
+                                                <?php endfor; ?>
+                                            </ul>
+                                        </div>
+
+                                        <input type="hidden" name="sort" id="sort-input">
+
+                                        <!-- Lowest price filter -->
+                                        <div class="d-inline-block">
+                                            <button id="lowest-price-filter" class="px-3 py-2 rounded-pill text-nowrap bg-secondary border small fw-medium text-center sort-btn" data-sort="price_asc" style="cursor: pointer;" value="">Lowest price</button>
+                                        </div>
+
+                                        <!-- Higher price filter -->
+                                        <div class="d-inline-block">
+                                            <button id="higher-price-filter" class="px-3 py-2 rounded-pill text-nowrap bg-secondary border small fw-medium text-center sort-btn" data-sort="price_desc" style="cursor: pointer;">Higher price</button>
+                                        </div>
+
+                                        <!-- Top rated filter -->
+                                        <div class="d-inline-block">
+                                            <button id="top-rated-filter" class="px-3 py-2 rounded-pill text-nowrap bg-secondary border small fw-medium text-center sort-btn" data-sort="top_rated" style="cursor: pointer;">Top rated</button>
+                                        </div>
+
+                                        <!-- Top sellers filter -->
+                                        <div class="d-inline-block">
+                                            <button id="top-sellers-filter" class="px-3 py-2 rounded-pill text-nowrap bg-secondary border small fw-medium text-center sort-btn" data-sort="top_sellers" style="cursor: pointer;">Top sellers</button>
+                                        </div>
+
+                                        <!-- Reset button -->
+                                        <div class="d-inline-block float-right">
+                                            <button id="reset" class="px-3 py-2 text-nowrap bg-black border small fw-medium text-center" style="cursor: pointer;">Reset</button>
+                                        </div>
+                                    </div>
+                                </div>
+                        </form>
+                    </div><!-- /.container-fluid -->
+                </section>
+                <section class="main-content">
+                    <div class="row" id="content-products">
+                        <?php foreach ($products as $product): ?>
+                            <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-4" id="products">
+                                <div class="product-card w-75 h-100">
+                                    <a href="<?php echo ROOT . "/" . strtolower($product->getCategory()->getName()) . "/" . $product->getSlug() ?>" class="product-link">
+                                        <div class="card-header h-20 bg-light">
+                                            <?php if ($product->getImage()) : ?>
+                                                <img src="<?php echo UPLOADS_IMAGES . '/' . $product->getImage() ?>"
+                                                    alt="<?php echo $product->getName() ?>" class="card-img-center h-100 w-100">
+                                            <?php endif; ?>
+                                        </div>
+                                        <div class="product-header">
+                                            <div class="product-price">
+                                                <span><?php echo $product->getPrice() ?>€</span>
+                                            </div>
+                                            <div class="ratings">
+                                                <span class="stars">
+                                                    <?php
+                                                    $averageRating = $reviews[$product->getId()]['average'];
+                                                    for ($i = 1; $i <= 5; $i++) {
+                                                        if ($i <= floor($averageRating)) {
+                                                            echo '<i class="fas fa-star"></i>';
+                                                        } elseif ($i - 1 < $averageRating && $i > floor($averageRating)) {
+                                                            echo '<i class="fas fa-star-half-alt"></i>';
+                                                        } else {
+                                                            echo '<i class="far fa-star"></i>';
+                                                        }
+                                                    }
+                                                    ?>
+                                                </span>
+                                                <span class="reviews-count"><?php echo $reviews[$product->getId()]['total'] ?> reviews</span>
+                                            </div>
+                                        </div>
+                                        <div class="card-body bg-white">
+                                            <div class="product-title"><?php echo $product->getName() ?></div>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+                        <?php endforeach; ?>
+                    </div>
+                </section>
+                <section class="pagination-content">
+                    <div class="container-fluid">
+                        <div class="row" id="pagination-card">
+                            <?php if ($totalPages > 1): ?>
+                                <?php
+                                $end = $currentPage !== 1 ? $records * $currentPage : $records;
+                                $start = $currentPage !== 1 ? $end - $records + 1 : 1;
+
+                                if ($totalPages === $currentPage) {
+                                    $end = $totalProducts;
+                                    $start = $totalProducts - $records + 1;
+                                }
+                                ?>
+                                <div class="col-md-6">
+                                    <div class="dataTables_info" id="tableProducts_info" role="status" aria-live="polite">
+                                        Showing <?php echo $start ?> to <?php $end; ?> of <?php echo $totalProducts; ?> entries
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="dataTables_paginate paging_simple_numbers" id="tableProducts_paginate">
+                                        <ul class="pagination">
+                                            <!-- Previous page -->
+                                            <?php if ($currentPage > 1): ?>
+                                                <li class="paginate_button page-item previous">
+                                                    <a href="#" data-page="<?php echo $currentPage - 1; ?>" aria-controls="tableProducts" class="page-link">Previous</a>
+                                                </li>
+                                            <?php else: ?>
+                                                <li class="paginate_button page-item previous disabled">
+                                                    <a href="#" aria-controls="tableProducts" class="page-link">Previous</a>
+                                                </li>
+                                            <?php endif; ?>
+
+                                            <!-- Numerated pages -->
+                                            <?php for ($i = 1; $i <= $totalPages; $i++): ?>
+                                                <li class="paginate_button page-item <?php echo $i === $currentPage ? 'active' : ''; ?>">
+                                                    <a href="#" data-page="<?php echo $i; ?>" aria-controls="tableProducts" class="page-link"><?php echo $i; ?></a>
+                                                </li>
+                                            <?php endfor; ?>
+
+                                            <!-- Next page -->
+                                            <?php if ($currentPage < $totalPages): ?>
+                                                <li class="paginate_button page-item next">
+                                                    <a href="#" data-page="<?php echo $currentPage + 1; ?>" aria-controls="tableProducts" class="page-link">Next</a>
+                                                </li>
+                                            <?php else: ?>
+                                                <li class="paginate_button page-item next disabled">
+                                                    <a href="#" aria-controls="tableProducts" class="page-link">Next</a>
+                                                </li>
+                                            <?php endif; ?>
+                                        </ul>
+                                    </div>
+                                </div>
+                            <?php endif; ?>
+                        </div>
+                    </div>
+                </section>
+            </div>
+            <!-- /.content-wrapper -->
+
+
+            <?php
+            require_once __DIR__ . "/layout/footer.php";
+            ?>
+
+
+        </div>
+
+        <script src="<?php echo ADMINLTE ?>plugins/jquery/jquery.min.js"></script>
+        <!-- Bootstrap 4 -->
+        <script src="<?php echo ADMINLTE ?>plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+        <!-- AdminLTE App -->
+        <script src="<?php echo ADMINLTE ?>dist/js/adminlte.min.js"></script>
+        <!-- Generic script for utilities -->
+        <script type="text/javascript" src="views/js/helper/utils.js"></script>
+        <!-- Ion Slider -->
+        <script src="<?php echo ADMINLTE ?>plugins/ion-rangeslider/js/ion.rangeSlider.min.js"></script>
+        <!-- Page specific script -->
+        <script type="text/javascript" src="views/js/categoryView.js"></script>
+    </body>
+
+    </html>
---
+++ views/js/categoryView.js
@@ -0,0 +1,318 @@
+$(document).ready(() => {
+  const form = getById("filter-form");
+  const search = document.querySelector(".search-product");
+  const sortInput = getById("sort-input");
+  const advancedFilters = document.querySelectorAll(".dropdown-toggle");
+  const sortButtons = document.querySelectorAll(".sort-btn");
+  const productsContainer = getById("content-products");
+  const paginationContainer = getById("pagination-card");
+  const url = new URLSearchParams(window.location.search);
+  const page = url.get("page") || 1;
+  const reset = getById("reset");
+  const debounceLoadProducts = debounce(function () {
+    loadProducts(1);
+    syncUIFromURL();
+  }, 1000);
+  const debounceSearchProducts = debounce(function () {
+    loadProducts(1);
+    syncUIFromURL();
+  }, 1000);
+
+  function syncUIFromURL() {
+    const params = new URLSearchParams(window.location.search);
+    form.querySelectorAll("input[type='checkbox']").forEach((input) => {
+      if (params.getAll(input.name).includes(input.value)) {
+        input.checked = true;
+      } else {
+        input.checked = false;
+      }
+    });
+
+    const currentSearch = params.get("search");
+    if (currentSearch) {
+      search.value = currentSearch;
+    }
+    const currentSort = params.get("sort");
+    if (currentSort) {
+      sortInput.value = currentSort;
+
+      sortButtons.forEach((btn) => {
+        btn.classList.remove("bg-primary");
+        btn.classList.add("bg-secondary");
+
+        if (btn.dataset.sort === currentSort) {
+          btn.classList.remove("bg-secondary");
+          btn.classList.add("bg-primary");
+        }
+      });
+    }
+    advancedFilters.forEach((btn) => {
+      btn.classList.remove("bg-primary");
+      btn.classList.add("bg-secondary");
+    });
+
+    if (params.get("range_price[]")) {
+      advancedFilters[0].classList.remove("bg-secondary");
+      advancedFilters[0].classList.add("bg-primary");
+    }
+    if (params.get("scores[]")) {
+      advancedFilters[1].classList.remove("bg-secondary");
+      advancedFilters[1].classList.add("bg-primary");
+    }
+  }
+
+  function loadProducts(page = null, pushState = true) {
+    const formData = new FormData(form);
+    const slider = $("#slider").data("ionRangeSlider");
+    if (slider) {
+      const defaultFrom = 0;
+      const defaultTo = 5000;
+      if (slider.result.from == defaultFrom && slider.result.to == defaultTo) {
+        formData.delete("range_price[]");
+      }
+    }
+
+    if (!formData.get("search")) {
+      formData.delete("search");
+    }
+
+    if (!formData.get("sort")) {
+      formData.delete("sort");
+    }
+    if (page && page > 1) {
+      formData.set("page", page);
+    } else {
+      formData.delete("page");
+    }
+
+    const params = new URLSearchParams(formData);
+    const query = params.toString();
+    const newUrl = query
+      ? `${window.location.pathname}?${query}`
+      : window.location.pathname;
+
+    if (pushState) {
+      history.pushState({}, "", newUrl);
+    }
+
+    fetch(newUrl, {
+      headers: {
+        "X-Requested-With": "XMLHttpRequest",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        renderProducts(data);
+        renderPagination(
+          data.totalPages,
+          data.currentPage,
+          data.totalProducts,
+          data.records,
+        );
+      })
+      .catch((err) => console.error(err));
+  }
+
+  function renderProducts(data) {
+    productsContainer.innerHTML = "";
+
+    if (!data.products.length) {
+      productsContainer.innerHTML = "<p>No products found</p>";
+      return;
+    }
+
+    data.products.forEach((product) => {
+      let starsHTML = "";
+
+      const avg = parseFloat(product.average) || 0;
+
+      for (let i = 1; i <= 5; i++) {
+        if (i <= Math.floor(avg)) {
+          starsHTML += `<i class="fas fa-star"></i>`;
+        } else if (i - 1 < avg && i > Math.floor(avg)) {
+          starsHTML += `<i class="fas fa-star-half-alt"></i>`;
+        } else {
+          starsHTML += `<i class="far fa-star"></i>`;
+        }
+      }
+
+      const imageHTML = product.image
+        ? `<img src="/Ecommerce/uploads/images/${product.image}"
+              alt="${product.name}"
+              class="card-img-center h-100 w-100">`
+        : "";
+
+      productsContainer.innerHTML += `
+      <div class="col-sm-3">
+        <div class="product-card w-75 h-100">
+          <a href="/Ecommerce/${product.category.toLowerCase()}/${product.slug}" class="product-link">
+            <div class="card-header h-20 bg-light">
+              ${imageHTML}
+            </div>
+
+            <div class="product-header">
+              <div class="product-price">
+                <span>${product.price}€</span>
+              </div>
+
+              <div class="ratings">
+                <span class="stars">
+                  ${starsHTML}
+                </span>
+                <span class="reviews-count">
+                  ${data.reviews[product.id].total || 0} reviews
+                </span>
+              </div>
+            </div>
+
+            <div class="card-body bg-white">
+              <div class="product-title">
+                ${product.name}
+              </div>
+            </div>
+          </a>
+        </div>
+      </div>
+    `;
+    });
+  }
+
+  function renderPagination(totalPages, currentPage, totalProducts, records) {
+    if (totalPages < 2) {
+      paginationContainer.innerHTML = "";
+      return;
+    }
+
+    let end = currentPage !== 1 ? records * currentPage : records;
+    let start = currentPage !== 1 ? end - records + 1 : 1;
+
+    if (totalPages === currentPage) {
+      end = totalProducts;
+      start = totalProducts - records + 1;
+    }
+
+    let html = `<div class="col-md-6">
+                  <div class="dataTables_info" id="tableProducts_info" role="status" aria-live="polite">
+                    Showing ${start} to ${end} of ${totalProducts} entries
+                  </div>
+                </div>
+                <div class="col-md-6">
+                  <div class="dataTables_paginate paging_simple_numbers" id="tableProducts_paginate">
+                    <ul class="pagination">
+                      <!-- Previous page -->`;
+
+    if (currentPage > 1) {
+      html += `<li class="paginate_button page-item previous">
+                  <a href="#" data-page="${currentPage - 1}" aria-controls="tableProducts" class="page-link">Previous</a>
+                </li>`;
+    } else {
+      html += `<li class="paginate_button page-item previous disabled">
+                  <a href="#" aria-controls="tableProducts" class="page-link">Previous</a>
+                </li> <!-- Numerated pages -->`;
+    }
+
+    for (let i = 1; i <= totalPages; i++) {
+      html += `<li class="paginate_button page-item ${i === currentPage ? "active" : ""}">
+                  <a href="#" data-page="${i}" aria-controls="tableProducts" class="page-link">${i}</a>
+                </li>`;
+    }
+
+    html += `<!-- Next page -->`;
+
+    if (currentPage < totalPages) {
+      html += `<li class="paginate_button page-item next">
+                  <a href="#" data-page="${currentPage + 1}" aria-controls="tableProducts" class="page-link">Next</a>
+              </li>`;
+    } else {
+      html += `<li class="paginate_button page-item next disabled">
+                  <a href="#" aria-controls="tableProducts" class="page-link">Next</a>
+                </li>`;
+    }
+
+    html += `</ul></div></div>`;
+    paginationContainer.innerHTML = html;
+  }
+
+  search.addEventListener("input", function (e) {
+    debounceSearchProducts();
+  });
+
+  sortButtons.forEach((button) => {
+    button.addEventListener("click", function (e) {
+      e.preventDefault();
+      sortInput.value = this.dataset.sort;
+
+      sortButtons.forEach((btn) => {
+        btn.classList.remove("bg-primary");
+        btn.classList.add("bg-secondary");
+      });
+
+      this.classList.remove("bg-secondary");
+      this.classList.add("bg-primary");
+
+      loadProducts(1);
+    });
+  });
+
+  form.querySelectorAll("input[type='checkbox']").forEach((input) => {
+    input.addEventListener("change", function () {
+      loadProducts(1);
+      syncUIFromURL();
+    });
+  });
+
+  $("#slider").on("change", function () {
+    debounceLoadProducts();
+  });
+
+  paginationContainer.addEventListener("click", function (e) {
+    if (e.target.matches("[data-page]")) {
+      e.preventDefault();
+      loadProducts(e.target.dataset.page);
+    }
+  });
+
+  reset.addEventListener("click", function (e) {
+    e.preventDefault();
+    window.location.href = window.location.pathname;
+  });
+
+  window.addEventListener("popstate", function () {
+    syncUIFromURL();
+    loadProducts(null, false);
+  });
+
+  setupIonSlider();
+  focusDropdown();
+  syncUIFromURL();
+  loadProducts(page, false);
+});
+
+function focusDropdown() {
+  $(document).on("click", ".dropdown-menu", function (e) {
+    e.stopPropagation();
+  });
+}
+
+function setupIonSlider() {
+  /* ION SLIDER */
+  $("#slider").ionRangeSlider({
+    min: 0,
+    max: 5000,
+    from: 0,
+    to: 5000,
+    type: "double",
+    step: 1,
+    prefix: "€",
+    prettify: false,
+    hasGrid: true,
+  });
+}
+
+function debounce(func, delay) {
+  let timeout;
+  return function () {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => func.apply(this, arguments), delay);
+  };
+}
