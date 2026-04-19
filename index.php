a9660d74f6f010fcd99a2eacb48a251c4bd11b61
ManuelLozano98 <lololozano1998@gmail.com>
2026-04-19 20:54:24 +0200

    feat: integrate DI container and middlewares and move error handler logic from index

--- index.php
+++ index.php
@@ -5,105 +5,21 @@ require_once __DIR__ . '/config/constants.php';
 use Slim\Factory\AppFactory;
 use App\Middleware\ApiMiddleware;
 use Middlewares\TrailingSlash;
-use Slim\Exception\HttpNotFoundException;
-use Slim\Exception\HttpMethodNotAllowedException;
-use Psr\Http\Message\ServerRequestInterface;
-use App\Exceptions\NotFoundException;
-use App\Exceptions\DuplicateException;
-use App\Exceptions\ForeignKeyException;
-use App\Exceptions\InsertException;
-use App\Exceptions\UpdateException;
-use App\Exceptions\DeleteException;
+use App\Middleware\SessionMiddleware;
+use App\Middleware\CartMiddleware;
+use App\Handlers\ErrorHandler;
 
+$container = require __DIR__ . '/container.php';
+AppFactory::setContainer($container);
 $app = AppFactory::create();
 
 $app->setBasePath('/Ecommerce'); // Set the base path to your project folder
 
+$app->add(SessionMiddleware::class);
 $app->addRoutingMiddleware();
 $errorMiddleware = $app->addErrorMiddleware(true, true, true);
+$errorMiddleware->setDefaultErrorHandler([ErrorHandler::class, 'handle']);
 
-$errorMiddleware->setDefaultErrorHandler(
-    function (
-        ServerRequestInterface $request,
-        Throwable $exception,
-        bool $displayErrorDetails
-    ) use ($app) {
-
-        $responseFactory = $app->getResponseFactory();
-        $response = $responseFactory->createResponse();
-
-        $path = $request->getUri()->getPath();
-        $isApi = str_contains($path, '/api/');
-
-        $status = 500;
-        $message = 'Internal Server Error';
-
-        // HTTP ERRORS
-        if ($exception instanceof HttpNotFoundException) {
-            $status = 404;
-            $message = 'Route not found';
-        }
-
-        if ($exception instanceof HttpMethodNotAllowedException) {
-            $status = 405;
-            $message = 'Method not allowed. Must be one of: ' . implode(', ', $exception->getAllowedMethods());
-        }
-
-        // SERVICE ERRORS
-        if ($exception instanceof NotFoundException) {
-            $status = 404;
-            $message = $exception->getMessage();
-        }
-
-        if (
-            $exception instanceof DuplicateException ||
-            $exception instanceof ForeignKeyException
-        ) {
-            $status = 409;
-            $message = $exception->getMessage();
-        }
-
-        if (
-            $exception instanceof InsertException ||
-            $exception instanceof UpdateException ||
-            $exception instanceof DeleteException
-        ) {
-            $status = 400;
-            $message = $exception->getMessage();
-        }
-
-        // API
-        if ($isApi) {
-            $response->getBody()->write(json_encode([
-                'error' => $message
-            ]));
-
-            return $response
-                ->withHeader('Content-Type', 'application/json')
-                ->withStatus($status);
-        }
-
-        // WEB
-        if ($status === 404) {
-            ob_start();
-            require __DIR__ . '/views/404.php';
-            $html = ob_get_clean();
-
-            $response->getBody()->write($html);
-            return $response->withStatus(404);
-        } else {
-            ob_start();
-            require __DIR__ . '/views/500.html';
-            $html = ob_get_clean();
-            $response->getBody()->write($html);
-            return $response->withStatus(500);
-        }
-
-        return $response
-            ->withHeader('Content-Type', 'text/html')
-            ->withStatus($status);
-    }
-);
 
 $app->group('/api', function ($group) {
     (require __DIR__ . '/routes/api.php')($group);
@@ -112,5 +28,6 @@ $app->group('/api', function ($group) {
 $app->add(new TrailingSlash(true)); // Prevent issues if user forgets the trailing slash by adding it
 
 (require __DIR__ . '/routes/web.php')($app);
+// $app->add(CartMiddleware::class);
 
 $app->run();
