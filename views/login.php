04b6fe00283975928759be03d4095ef6a385fb8b
ManuelLozano98 <lololozano1998@gmail.com>
2026-04-21 20:24:18 +0200

    feat: add login view and authentication logic

---
+++ views/login.php
@@ -0,0 +1,93 @@
+<?php
+require_once __DIR__ . '/../config/constants.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title><?php echo SITE ?> | Log in</title>
+
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="<?php echo ADMINLTE ?>plugins/fontawesome-free/css/all.min.css">
+    <!-- icheck bootstrap -->
+    <link rel="stylesheet" href="<?php echo ADMINLTE ?>plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="<?php echo ADMINLTE ?>dist/css/adminlte.min.css">
+    <link rel="stylesheet" href="<?php echo ADMINLTE ?>plugins/sweetalert2/sweetalert2.min.css">
+    <link rel="stylesheet" href="<?php echo ADMINLTE ?>plugins/toastr/toastr.min.css">
+</head>
+
+<body class="hold-transition login-page">
+    <div class="login-box">
+        <div class="login-logo">
+            <a href="<?php echo ROOT ?>"><?php echo SITE ?></a>
+        </div>
+        <div class="card">
+            <div class="card-body login-card-body">
+                <p class="login-box-msg">Sign in to start your session</p>
+                <form id="loginForm" method="post">
+                    <div class="input-group mb-3">
+                        <input id="username" type="text" class="form-control" placeholder="Email or username" required>
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span class="fas fa-envelope"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="input-group mb-3">
+                        <input id="password" type="password" class="form-control" placeholder="Password" required>
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span class="fas fa-lock"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-8">
+                            <div class="icheck-primary">
+                                <input type="checkbox" id="remember">
+                                <label for="remember">
+                                    Remember Me
+                                </label>
+                            </div>
+                        </div>
+                        <!-- /.col -->
+                        <div class="col-4">
+                            <button type="submit" class="btn btn-primary btn-block">Sign In</button>
+                        </div>
+                        <!-- /.col -->
+                    </div>
+                </form>
+
+
+                <p class="mb-1">
+                    <a href="<?php echo ROOT ?>/forgot-password">I forgot my password</a>
+                </p>
+                <p class="mb-0">
+                    Dont't have an account?
+                    <a href="<?php echo ROOT ?>/sign-up" class="text-center">Sign up</a>
+                </p>
+            </div>
+            <!-- /.login-card-body -->
+        </div>
+    </div>
+    <!-- /.login-box -->
+
+    <!-- jQuery -->
+    <script src="<?php echo ADMINLTE ?>plugins/jquery/jquery.min.js"></script>
+    <!-- Bootstrap 4 -->
+    <script src="<?php echo ADMINLTE ?>plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- AdminLTE App -->
+    <script src="<?php echo ADMINLTE ?>plugins/sweetalert2/sweetalert2.min.js"></script>
+    <script src="<?php echo ADMINLTE ?>plugins/toastr/toastr.min.js"></script>
+    <script src="<?php echo ADMINLTE ?>dist/js/adminlte.min.js"></script>
+    <script src="<?php echo ROOT . "/" ?>views/js/helper/utils.js"></script>
+    <script src="<?php echo ROOT . "/" ?>views/js/login.js"></script>
+</body>
+
+</html>
