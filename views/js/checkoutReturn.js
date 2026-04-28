1c8836b366b716e8e12be427801d87e8c5153d03
ManuelLozano98 <lololozano1998@gmail.com>
2026-04-28 21:33:03 +0200

    feat: add checkout return page

---
+++ views/js/checkoutReturn.js
@@ -0,0 +1,23 @@
+const BASE_URL = "/Ecommerce";
+initialize();
+
+async function initialize() {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  const sessionId = urlParams.get("session_id");
+  const response = await fetch(`${BASE_URL}/checkout-status/`, {
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    method: "POST",
+    body: JSON.stringify({ session_id: sessionId }),
+  });
+  const session = await response.json();
+
+  if (session.status == "complete") {
+    document.getElementById("success").classList.remove("hidden");
+    document.getElementById("customer-email").textContent =
+      session.customer_email;
+  }
+}
