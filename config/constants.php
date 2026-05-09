82f5c001ee86c0932cda2acbec46b9d37be2d98e
ManuelLozano98 <lololozano1998@gmail.com>
2026-05-09 16:07:49 +0200

    fix: correct path

--- config/constants.php
+++ config/constants.php
@@ -4,7 +4,7 @@ define("ROOT", "/Ecommerce");
 define("PUBLIC_DIRECTORY", "/public");
 define("ROOT_PUBLIC_DIRECTORY", ROOT . PUBLIC_DIRECTORY);
 define("ADMINLTE", ROOT_PUBLIC_DIRECTORY . "/adminlte/");
-define("HOME", ROOT . "/home");
+define("HOME", ROOT);
 define("UPLOADS", ROOT . "/uploads");
 define("UPLOADS_IMAGES", UPLOADS . "/images");
 define("UPLOADS_USERS_IMAGES", UPLOADS_IMAGES . "/users");
